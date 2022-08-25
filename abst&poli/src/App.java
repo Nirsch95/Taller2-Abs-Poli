68df23bdb469ca40aede98cc162064b31daf4a77
Nirsch95 <castromanzano95@gmail.com>
2022-08-24 21:23:36 -0500

    Actualización

--- abst&poli/src/App.java
+++ abst&poli/src/App.java
@@ -1,13 +1,40 @@
+/**
+ * Representa el codigo principal de ejecución
+ * Importa las librerias pertinentes
+ * @author Andrés Castro
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
+    /**
+     * La clase principal de ejecución del codigo
+     * 
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+        /**
+         * Variables utilizada para decidir lo que hacer en el cuerpo del codigo
+         */
         int opcion, fopcion = 0;
+        /**
+         * Variables que almacenan los valores necesarios para enviarle a los objetos
+         */
         String name;
         Integer year;
+        /**
+         * Array que almacena todos los objetos creados tipo nave espacial
+         */
         ArrayList<Spacevehicule> Spacevehicules = new ArrayList<>();
+        /**
+         * Importamos la clase Scanner para recibir información por teclado
+         */
         Scanner input = new Scanner(System.in);
+        /**
+         * Estructura de control do para ejecutar el codigo las veces que el usuario lo
+         * desee
+         */
         do {
             System.out.println("Bienvenido");
             System.out.println("Vamos a crear diferentes naves de su elección y datos proporcionados por usted.");
@@ -16,6 +43,9 @@ public class App {
             System.out.println("2. Nave no tripulada");
             System.out.println("3. Nave tripulada");
             opcion = input.nextInt();
+            /**
+             * Estructura de desición para hacer la acción que desea realizar el usuario
+             */
             switch (opcion) {
                 case 1:
                     System.out.println("Nombre de la nave");
@@ -32,7 +62,14 @@ public class App {
                     Integer push = input.nextInt();
                     System.out.println("País del cual fue fabricado");
                     String country = input.next();
+                    /**
+                     * Creamos la nave espacial utilizando el constructor necesario con las
+                     * variables necesarias para su creación
+                     */
                     Spacevehicule shuttle = new Shuttle(name, year, load, weight, height, push, country);
+                    /**
+                     * Almacenamos la nave creada
+                     */
                     Spacevehicules.add(shuttle);
                     break;
                 case 2:
@@ -46,7 +83,14 @@ public class App {
                     String association = input.next();
                     System.out.println("Se encuentra actualmente activo");
                     boolean actived = input.nextBoolean();
+                    /**
+                     * Creamos la nave espacial utilizando el constructor necesario con las
+                     * variables necesarias para su creación
+                     */
                     Spacevehicule unmanned = new Unmanned(name, year, inition, association, actived);
+                    /**
+                     * Almacenamos la nave creada
+                     */
                     Spacevehicules.add(unmanned);
                     break;
                 case 3:
@@ -60,18 +104,31 @@ public class App {
                     Integer orbit = input.nextInt();
                     System.out.println("Se encuentra actualmente activo");
                     String country2 = input.next();
+                    /**
+                     * Creamos la nave espacial utilizando el constructor necesario con las
+                     * variables necesarias para su creación
+                     */
                     Spacevehicule manned = new Manned(name, year, weight2, orbit, country2);
+                    /**
+                     * Almacenamos la nave creada
+                     */
                     Spacevehicules.add(manned);
                     break;
-                case 0:
-                    System.out.println("Gracias por usar nuestro sistema, ¡Hasta pronto!");
-                    break;
                 default:
+                    /**
+                     * Mensaje que aparece en caso de no elegir una opcion correcta
+                     */
                     System.out.println("Esa opción elegida no esta disponible");
             }
+            /**
+             * Decisión para acabar el programa
+             */
             System.out.println("¿Desea continuar?");
             System.out.println("(1)Si (0)No");
             fopcion = input.nextInt();
+            if (fopcion == 0) {
+                System.out.println("Gracias por usar nuestro sistema, ¡Hasta pronto!");
+            }
         } while (fopcion != 0);
     }
 }
--- abst&poli/src/ISpacevehicule.java
+++ abst&poli/src/ISpacevehicule.java
@@ -1,18 +1,25 @@
 /**
- * Representa la interface que complementa los comportamientos de unos tipos de nave
+ * Representa la interface que complementa los comportamientos de unos tipos de
+ * nave
+ * 
  * @author Andrés Castro
  */
 public interface ISpacevehicule {
     /**
-     * Representa el metodo para identificar si tiene o no la capacidad de llevar tripulación
+     * Representa el metodo para identificar si tiene o no la capacidad de llevar
+     * tripulación
+     * 
      * @param crew Tripulación de la nave
-     * @return Retorna la cantidad de tripulación 
+     * @return Retorna la cantidad de tripulación
      */
-    public Integer crewCapacity (Integer crew);
+    public Integer crewCapacity(Integer crew);
+
     /**
-     * Representa el metodo para identificar si tiene o no la capacidad de llevar oxigeno para la tripulación
-     * @param oxigen Oxigeno para la tripulación 
+     * Representa el metodo para identificar si tiene o no la capacidad de llevar
+     * oxigeno para la tripulación
+     * 
+     * @param oxigen Oxigeno para la tripulación
      * @return Retorna la cantidad de oxigeno que necesita para la tripulación
      */
-    public Integer oxigenCapacity (Integer oxigen); 
+    public Integer oxigenCapacity(Integer oxigen);
 }
